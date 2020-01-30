eba70aa060b26acfafd9354c2d0c780077bf6125
oleolema <1075056431@qq.com>
2020-01-30 14:33:29 +0800

    Fixed firefox can't open it

--- Animation.js
+++ Animation.js
@@ -1,43 +1,40 @@
-class Animation {
-    static colors = {
-        '-1': "rgb(205, 193, 180)",
-        0: "#00000000",
-        2: "#fbddb4",
-        4: "rgb(255, 191, 106)",
-        8: "rgb(236, 135, 0)",
-        16: "#ec931c",
-        32: "#f58b84",
-        64: "#fd4d40",
-        128: "#a844b9",
-        256: "#673AB7",
-        512: "#03A9F4",
-        1024: "#ead416",
-        2048: "#607D8B",
-        // 4096: "#4CAF50",
-        // 8192: "#CDDC39",
-        // 16384: "#FFEB3B",
-        // 32768: "#FF9800",
-        // 65536: "#FF5722",
-        'other': "#795548",
-    };
+function Animation(table) {
+    this.table = table;
+}
 
-    constructor(table) {
-        this.table = table;
+Animation.colors = {
+    '-1': "rgb(205, 193, 180)",
+    0: "#00000000",
+    2: "#fbddb4",
+    4: "rgb(255, 191, 106)",
+    8: "rgb(236, 135, 0)",
+    16: "#ec931c",
+    32: "#f58b84",
+    64: "#fd4d40",
+    128: "#a844b9",
+    256: "#673AB7",
+    512: "#03A9F4",
+    1024: "#ead416",
+    2048: "#607D8B",
+    // 4096: "#4CAF50",
+    // 8192: "#CDDC39",
+    // 16384: "#FFEB3B",
+    // 32768: "#FF9800",
+    // 65536: "#FF5722",
+    'other': "#795548",
+};
+
+
+Animation.getFontColor = (val) => {
+    if (typeof Animation.colors[val] === 'undefined') {
+        return main.getConTrastColor(Animation.colors['other']).color;
     }
+    return main.getConTrastColor(Animation.colors[val]).color;
+}
 
-    static getFontColor(val) {
-        if (typeof this.colors[val] === 'undefined') {
-            return main.getConTrastColor(this.colors['other']).color;
-        }
-        return main.getConTrastColor(this.colors[val]).color;
+Animation.getColor = (val) => {
+    if (typeof Animation.colors[val] === 'undefined') {
+        return Animation.colors['other'];
     }
-
-    static getColor(val) {
-        if (typeof this.colors[val] === 'undefined') {
-            return this.colors['other'];
-        }
-        return this.colors[val];
-    }
-
-
+    return Animation.colors[val];
 }
